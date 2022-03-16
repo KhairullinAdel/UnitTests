0d3f124999c6900d51102b900c2f5587c7b74d3f
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-03-16 11:00:25 +0300

    MyQueue

--- UnitTests/Containers.cs
+++ UnitTests/Containers.cs
@@ -90,6 +90,11 @@ namespace UnitTests
             arr = new DynArray();
         }
 
+        public DynArray GetArray()
+        {
+            return arr;
+        }
+
         public bool IsEmpty()
         {
             return arr.GetLength() == 0;
